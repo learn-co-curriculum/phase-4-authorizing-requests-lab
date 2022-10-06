4d0767cd6b5d166f80272a2371585342ded44476
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-06 14:37:26 +0300

    commit tree 46c6e2dccdd2d3521e245c93af81ef9364dfcf54

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,6 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -12,6 +13,10 @@ class MembersOnlyArticlesController < ApplicationController
   end
 
   private
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
