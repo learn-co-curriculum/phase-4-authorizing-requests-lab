08695e965f1c98dddfe5be602ee1907ce0c2c855
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-06-30 12:34:10 -0700

    Authorizing users

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,7 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authenticate_user
+  before_action :set_article, only: [:show]
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -7,14 +9,25 @@ class MembersOnlyArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    render json: @article
   end
 
   private
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+  def authenticate_user
+    return if user_signed_in?
+
+    render json: { error: 'Unauthorized' }, status: :unauthorized
   end
 
+  def set_article
+    @article = Article.find(params[:id])
+    return if @article.is_member_only?
+
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
+
+  def record_not_found
+    render json: { error: 'Article not found' }, status: :not_found
+  end
 end
